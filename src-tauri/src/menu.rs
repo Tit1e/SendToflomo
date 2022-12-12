1288bc0a59c7093f143c09e72f0e0005e17090a8
Tit1e <evollyone@outlook.com>
2022-12-12 23:01:38 +0800

    菜单新增重启应用功能

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,16 +1,23 @@
-use tauri::utils::assets::EmbeddedAssets;
+use tauri::{
+  utils::assets::EmbeddedAssets,Manager
+};
 use tauri::{Context, CustomMenuItem, Menu, Submenu, WindowMenuEvent, MenuItem};
 
 // 应用菜单项
 pub fn init(_context: &Context<EmbeddedAssets>) -> Menu {
     // 应用名称
-    let name = &_context.package_info().name;
+    // let name = &_context.package_info().name;
     let version = &_context.package_info().version;
-    let about = format!("{} v{}", name, version);
+    let about = format!("Version: {}", version);
     let app_menu = Submenu::new(
         "",
         Menu::new()
           .add_item(CustomMenuItem::new("about".to_string(), about).disabled().into())
+          .add_native_item(MenuItem::Separator)
+          .add_item(
+            CustomMenuItem::new("restart".to_string(), "重启应用")
+            .accelerator("CmdOrCtrl+Shift+R"),
+          )
           .add_native_item(MenuItem::Separator)
           .add_item(CustomMenuItem::new("quit".to_string(), "退出").accelerator("CmdOrCtrl+Q").into())
     );
@@ -20,12 +27,15 @@ pub fn init(_context: &Context<EmbeddedAssets>) -> Menu {
 }
 
 // 应用菜单处理事件
-pub fn handler(event: WindowMenuEvent) {
-    // 匹配菜单 id
-    match event.menu_item_id() {
-        "quit" => {
-            std::process::exit(0);
-        }
-        _ => {}
+pub fn handler(event: WindowMenuEvent<tauri::Wry>) {
+  // 匹配菜单 id
+  let win = Some(event.window()).unwrap();
+  let app = win.app_handle();
+  match event.menu_item_id() {
+    "restart" => tauri::api::process::restart(&app.env()),
+    "quit" => {
+      std::process::exit(0);
     }
+    _ => {}
+  }
 }
