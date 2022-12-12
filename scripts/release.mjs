e246fe1c93daa905103962e5e9804907715c5855
Tit1e <evollyone@outlook.com>
2022-12-12 16:19:27 +0800

    版本号同步 tauri.conf.json

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -9,6 +9,7 @@ const require = createRequire(import.meta.url);
 async function release() {
   const flag = process.argv[2] ?? 'patch';
   const packageJson = require('../package.json');
+  const tauriJson = require('../src-tauri/tauri.conf.json');
   let [a, b, c] = packageJson.version.split('.').map(Number);
 
   if (flag === 'major') {  // 主版本
@@ -27,15 +28,18 @@ async function release() {
 
   const nextVersion = `${a}.${b}.${c}`;
   packageJson.version = nextVersion;
+  tauriJson.package.version = nextVersion;
 
   const nextTag = `v${nextVersion}`;
   await updatelog(nextTag, 'release');
 
   // 将新版本写入 package.json 文件
-  fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+  // fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+  // 将新版本写入 tauri.conf.json 文件
+  fs.writeFileSync('./src-tauri/tauri.conf.json', JSON.stringify(tauriJson, null, 2));
 
   // 提交修改的文件，打 tag 标签（tag 标签是为了触发 github action 工作流）并推送到远程
-  execSync('git add ./package.json ./UPDATE_LOG.md');
+  execSync('git add ./package.json ./UPDATE_LOG.md ./scr-tauri/tauri.conf.json');
   execSync(`git commit -m "v${nextVersion}"`);
   execSync(`git tag -a v${nextVersion} -m "v${nextVersion}"`);
   execSync(`git push`);
