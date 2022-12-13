009e3b041509402835d93c6802c25ab90029450a
Tit1e <evollyone@outlook.com>
2022-12-13 11:24:14 +0800

    还原更新配置

--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ import VueI18n from './language'
 import store from '@/store/index.js'
 
 import '@/assets/css/index.scss'
+
+import { checkUpdate } from '@tauri-apps/api/updater';
+checkUpdate().then(res => {
+  console.log(res)
+}).catch(e => {
+  console.log(e)
+})
+console.log(111)
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(VueI18n)
