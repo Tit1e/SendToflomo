911bafb5588f0c3eccd2a04272e2ea800848e1f8
Tit1e <evollyone@outlook.com>
2022-12-18 00:52:29 +0800

    docs📝:  添加迁移文档

--- src/components/Help/index.vue
+++ src/components/Help/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="help">
+    <div class="help-title">{{t('data-migration')}}</div>
+    <div class="text-danger text-indent">{{t('data-migration-tip')}}</div>
+    <ol>
+      <li>{{t('data-migration-1')}} <a class="link pointer bold underline text-highlight" @click="handleDownload">Send2Flomo v2.5.5</a></li>
+      <li>{{t('data-migration-2')}}</li>
+      <li>{{t('data-migration-3')}}</li>
+    </ol>
     <div class="help-title">{{t('warning')}}</div>
     <ol>
-      <li>{{t('warning-1')}}</li>
-      <li>{{t('warning-2')}}</li>
-      <li>{{t('warning-3')}}</li>
-      <li>{{t('warning-4')}}</li>
-      <li>{{t('warning-5')}}</li>
-      <li>{{t('warning-6')}}</li>
-      <li>{{t('warning-7')}}</li>
+      <li v-for="i in 6">{{t(`warning-${i + 1}`)}}</li>
     </ol>
     <div class="help-title">{{t('file')}}</div>
     <ol>
@@ -33,7 +34,7 @@
     <div class="mb-6 help-title">
       {{t('why-few-book')}}
     </div>
-    <div class="mb-10 pl-20">
+    <div class="mb-10 text-indent">
       {{ t('handle-few-book') }}
     </div>
     <div class="help-title mb-10">{{t('other-questions')}}</div>
@@ -50,6 +51,9 @@
 import { openUrl } from '@/utils/utils.js'
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
+const handleDownload = () => {
+  openUrl('https://github.com/Tit1e/kindle2Flomo/releases/download/v2.5.5/Send2flomo-2.5.5.dmg')
+}
 </script>
 
 <style lang="scss" scoped>
